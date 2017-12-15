f593e324f8c8e5415ea15218b96fb3b24cfe7dff
alpatinho <lucaasberg@gmail.com>
2017-12-14 23:17:09 -0200

    Removendo paredes

--- MazeGenerator/sketch.js
+++ MazeGenerator/sketch.js
@@ -1,40 +1,60 @@
-var larguraBloco;
-var numLinhas;
-var numColunas;
+var larguraBloco = 40;
+var numColunas, numLinhas;
 var grid = [];
 var current;
+var stack = [];
 
 function setup() {
-	var canvas = createCanvas(400, 400);
-	larguraBloco = 40;
+	createCanvas(400, 400);
 	numLinhas = floor(width/larguraBloco);
 	numColunas = floor(height/larguraBloco);
-	frameRate(20);
+	frameRate(10);
 
 	// Instancia um bloco em cada posicao do grid
-	for(var i = 0; i < numColunas; i++) {
-		for(var j = 0; j < numLinhas; j++) {
-			var bloco = new Bloco(j,i);
-			grid.push(bloco);
-		}
-	}
+	for (var   j = 0; j < numLinhas; j++) {
+    for (var i = 0; i < numColunas; i++) {
+      var bloco = new Bloco(i, j);
+      grid.push(bloco);
+    }
+  }
 
 	current = grid[0];
-
 }
 
 function draw() {
 	background(90);
+
 	for(var i = 0; i < grid.length; i++) {
 			grid[i].printaBloco();
-		}
-
+	}
 
 	current.visitado = true;
 	var next = current.getNeighbors();
 	if(next) {
 		next.visited = true;
+		removeParedes(current, next);
 		current = next;
+	}
+
+
+}
+
+function removeParedes(current,next) {
+	var x = current.x - next.x;
+	if(x == 1) {
+		current.paredes[3] = false;
+		next.paredes[1] = false;
+	} else if(x == -1) {
+		current.paredes[1] = false;
+		next.paredes[3] = false;
+	}
+	var y = current.y - next.y;
+	if(y == 1) {
+		current.paredes[0] = false;
+		next.paredes[2] = false;
+	} else if(y == -1) {
+		current.paredes[2] = false;
+		next.paredes[0] = false;
 	}
 }
 
@@ -54,6 +74,7 @@ function Bloco(x, y) {
 
 	this.getNeighbors = function() {
 		var neighbors = [];
+
 		var top    = grid[index(x, y-1)];
 		var right  = grid[index(x+1, y)];
 		var bottom = grid[index(x, y+1)];
@@ -87,24 +108,23 @@ function Bloco(x, y) {
 		var j = this.y*aux;
 		// printa as paredes de um bloco
 		stroke(255);
-		//noFill();
-		//rect(i,j,aux,aux);
 
 		if(this.paredes[0]) {
 			line(		i			,			j		,	i + aux	,		j 		);
 		}
 		if(this.paredes[1]) {
-			line(		i 		,	j + aux	,	i				,		j			);
+			line(		i 	+ aux 	,	j	,	i + aux	,		j + aux	);
 		}
 		if(this.paredes[2]) {
-			line(i + aux	,	   j  	,	i				,		j   	);
+			line(i + aux	,	   j + aux 	,	i		,		j + aux  	);
 		}
 		if(this.paredes[3]) {
-			line(i + aux	,	j + aux	,	i				,	j + aux	);
+			line(i 	,	j + aux	,	i		,	j );
 		}
 
 		if(this.visitado) {
-			fill(0,0,255,100);
+			noStroke();
+			fill(0, 0, 255, 100);
 			rect(i,j,aux,aux);
 		}
 
