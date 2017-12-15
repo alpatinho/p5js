83f92db89fc56a5934cfc16994b9425a42199b09
alpatinho <lucaasberg@gmail.com>
2017-12-14 22:33:22 -0200

    Caminhando pelo grid

--- MazeGenerator/sketch.js
+++ MazeGenerator/sketch.js
@@ -1,44 +1,85 @@
 var larguraBloco;
 var numLinhas;
 var numColunas;
-var grid;
+var grid = [];
+var current;
 
 function setup() {
 	var canvas = createCanvas(400, 400);
 	larguraBloco = 40;
 	numLinhas = floor(width/larguraBloco);
 	numColunas = floor(height/larguraBloco);
-	grid = new Array(numColunas);
+	frameRate(20);
 
-	// Para cada coluna do grid, cria um array de linhas
-	for(var i = 0; i < numColunas; i++) {
-		grid[i] = new Array(numLinhas);
-	}
-
+	// Instancia um bloco em cada posicao do grid
 	for(var i = 0; i < numColunas; i++) {
 		for(var j = 0; j < numLinhas; j++) {
-			var bloco = new Bloco(i,j);
-			grid[i][j] = bloco;
-			//console.log(grid[i][j]);
+			var bloco = new Bloco(j,i);
+			grid.push(bloco);
 		}
 	}
+
+	current = grid[0];
 
 }
 
 function draw() {
-	background(40);
-	for(var i = 0; i < numColunas; i++) {
-		for(var j = 0; j < numLinhas; j++) {
-			grid[i][j].printaBloco();
+	background(90);
+	for(var i = 0; i < grid.length; i++) {
+			grid[i].printaBloco();
 		}
+
+
+	current.visitado = true;
+	var next = current.getNeighbors();
+	if(next) {
+		next.visited = true;
+		current = next;
 	}
+}
+
+function index(i, j) {
+	if(i < 0 || j < 0 || i > numColunas - 1 || j > numLinhas - 1) {
+		return -1;
+	}
+	return i + j * numColunas;
 }
 
 // Objeto bloco
 function Bloco(x, y) {
 	this.x = x;
 	this.y = y;
-	this.parede = [true,true,true,true];
+	this.paredes = [true,true,true,true];
+	this.visistado = false;
+
+	this.getNeighbors = function() {
+		var neighbors = [];
+		var top    = grid[index(x, y-1)];
+		var right  = grid[index(x+1, y)];
+		var bottom = grid[index(x, y+1)];
+		var left   = grid[index(x-1, y)];
+
+		if(top && !top.visited) {
+			neighbors.push(top);
+		}
+		if(right && !right.visited) {
+			neighbors.push(right);
+		}
+		if(bottom && !bottom.visited) {
+			neighbors.push(bottom);
+		}
+		if(left && !left.visited) {
+			neighbors.push(left);
+		}
+
+		if(neighbors.length > 0) {
+			var r = floor(random(0,neighbors.length));
+			return neighbors[r];
+		} else {
+			return undefined;
+		}
+
+	}
 
 	this.printaBloco = function() {
 		var aux = larguraBloco;
@@ -46,18 +87,26 @@ function Bloco(x, y) {
 		var j = this.y*aux;
 		// printa as paredes de um bloco
 		stroke(255);
-		noFill();
-		if(this.parede[0]) {
-			line(i,j,i + aux,j);
+		//noFill();
+		//rect(i,j,aux,aux);
+
+		if(this.paredes[0]) {
+			line(		i			,			j		,	i + aux	,		j 		);
 		}
-		if(this.parede[1]) {
-			line(i,j + aux,i,j);
+		if(this.paredes[1]) {
+			line(		i 		,	j + aux	,	i				,		j			);
 		}
-		if(this.parede[2]) {
-			line(i + aux,j,i,j);
+		if(this.paredes[2]) {
+			line(i + aux	,	   j  	,	i				,		j   	);
 		}
-		if(this.parede[3]) {
-			line(i + aux,j + aux,i,j + aux);
+		if(this.paredes[3]) {
+			line(i + aux	,	j + aux	,	i				,	j + aux	);
 		}
+
+		if(this.visitado) {
+			fill(0,0,255,100);
+			rect(i,j,aux,aux);
+		}
+
 	}
 }
